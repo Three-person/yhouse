da7b52e440d1e51465cef9f808ebcd38af4ed1c5
huahua <1064945472@qq.com>
2017-03-01 21:08:55 +0800

    '此次添加了夜生活组件'

--- component/nightlife/nightlife.js
+++ component/nightlife/nightlife.js
@@ -1,5 +1,31 @@
 angular.module('nightlifeModule',['ui.router'])
-
-.controller('nightlifeCtrl',['$scope',function($scope){
-	
+.service('liveBanData',['$http',function($http){
+	this.get=function(){
+		return $http.get('data/banner3.json');
+	}
+}])
+.service('liveData',['$http',function($http){
+	this.get=function(){
+		return $http.get('data/main3.json');
+	}
+}])
+.controller('nightlifeCtrl',['$scope','$http','liveBanData','liveData',function($scope,$http,liveBanData,liveData){
+	liveBanData.get().success(function(res){
+		$scope.arr=res.data.reserveList[0].content;	
+	});
+	liveData.get().success(function(res){
+		var arr1=res.data.doc[2].itemData[0].list;
+		var title=res.data.doc[2].itemData[0].title;
+		var viceTitle=res.data.doc[2].itemData[0].viceTitle;
+		$scope.title=title;
+		$scope.viceTitle=viceTitle;
+		var contentsNumStr=res.data.doc[2].itemData[0].contentsNumStr;
+		$scope.contentsNumStr=contentsNumStr;
+		$scope.arr1=arr1;
+		
+		var itemData=res.data.doc[3].itemData;
+		$scope.itemData=itemData;
+		console.log(itemData);
+		
+	});
 }])
