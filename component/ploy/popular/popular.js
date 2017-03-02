3cca9752ff4d427d0693545b514a43530cf0dfe2
zhanghongyuan <zhy5205318@sina.com>
2017-03-02 17:54:35 +0800

    '二级页面ploy'

--- component/ploy/popular/popular.js
+++ component/ploy/popular/popular.js
@@ -1,13 +1,13 @@
-angular.module('popularModule',['ui.router'])
+angular.module('popularModule',['ui.router','angularCSS'])
 
 .config(function($stateProvider,$urlRouterProvider){
     $urlRouterProvider.otherwise('/ploy');
     $stateProvider
-    .state('ploy.popular',{
+    .state('popular',{
         url:'/popular',
         templateUrl:'component/ploy/popular/popular.html',
         controller:'popularCtrl',
-        css:'component/ploy/popular/popular.css'
+        css: 'component/ploy/popular/popular.css'
     })
 })
 
@@ -19,7 +19,10 @@ angular.module('popularModule',['ui.router'])
 
 .controller('popularCtrl',['$scope','popularData',function($scope,popularData) {
     popularData.get().success(function (res) {
-        $scope.popularArr = res.data.contentList;
-        console.log($scope.popularArr);
+        $scope.headArr = res.data;
+        $scope.shopArr = res.data.contentList;
+        $scope.des = res.data.topicDescription;
+        $scope.des = $($scope.des);
+        console.log($scope.des);
     })
 }])
