b27e14dab410e3efc8ed1ee85ea7a11ddc06a218
zhanghongyuan <zhy5205318@sina.com>
2017-03-04 19:11:21 +0800

    '又错了'

--- component/ploy/ploydetail/ploydetail.js
+++ component/ploy/ploydetail/ploydetail.js
@@ -1,4 +1,4 @@
-angular.module('ploydetailModule',['ui.router','angularCSS','buyNowModule'])
+angular.module('ploydetailModule',['ui.router','angularCSS'])
 
 .config(function($stateProvider,$urlRouterProvider){
     $urlRouterProvider.otherwise('/ploy');
