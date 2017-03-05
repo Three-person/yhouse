6a4e557593e1a660446d209ea775b278ab1053e7
zhanghongyuan <zhy5205318@sina.com>
2017-03-05 18:48:21 +0800

    '添加立即购买页面'

--- component/ploy/ploydetail/ploydetail.js
+++ component/ploy/ploydetail/ploydetail.js
@@ -1,4 +1,4 @@
-angular.module('ploydetailModule',['ui.router','me-lazyload','angularCSS'])
+angular.module('ploydetailModule',['ui.router','me-lazyload','angularCSS','buyNowModule'])
 
 .config(function($stateProvider,$urlRouterProvider){
     $urlRouterProvider.otherwise('/ploy');
@@ -24,6 +24,7 @@ angular.module('ploydetailModule',['ui.router','me-lazyload','angularCSS'])
     for (var i = 0; i < id.length; i++){
         ploydetailData.get('http://m.yhouse.com/api/m/event/item-v2.3/'+id[i]+'?from=h5').success(function (res) {
             $scope.headArr = res.data;
+            console.log($scope.headArr);
             $scope.reason = res.data.highs;
             $scope.detail = res.data.content;
             $scope.detail = $($scope.detail);
@@ -32,7 +33,11 @@ angular.module('ploydetailModule',['ui.router','me-lazyload','angularCSS'])
             })
             $scope.attention = res.data.productLabels;
             $scope.tips = res.data.productAdvice;
-            console.log($scope.tips);
+
+            $scope.proImg = res.data.mPicUrl;
+            $scope.proName = res.data.title;
+            $scope.proPrice = res.data.neededCredits;
+            sessionStorage.setItem('buyNow',JSON.stringify([$scope.proImg,$scope.proName,$scope.proPrice]));
         })
     }
 }])
